4a40e75228694242a7064733d0c90a3293978bd9
Tiago Bohnenberger <tiagoboh10@gmail.com>
2021-07-19 18:25:40 -0300

    Auditing data

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_category (NAME) VALUES ('Livros');
-INSERT INTO tb_category (NAME) VALUES ('Eletrônicos');
-INSERT INTO tb_category (NAME) VALUES ('Computadores');
+INSERT INTO tb_category (NAME, created_At) VALUES ('Livros', NOW());
+INSERT INTO tb_category (NAME, created_At) VALUES ('Eletrônicos', NOW());
+INSERT INTO tb_category (NAME, created_At) VALUES ('Computadores', NOW());
