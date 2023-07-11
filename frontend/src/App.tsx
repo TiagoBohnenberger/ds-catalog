3e32f855927705aadd7097677b72a5b4a6054b14
Tiago Bohnenberger <tiago.bohnenberger@syonet.com>
2023-07-10 22:20:27 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,12 @@
-import Navbar from 'components/Navbar';
 import './assets/styles/custom.scss';
 import './App.css';
-import Home from 'pages/Home';
 
+import Routes from 'Routes';
 
 function App() {
   return (
     <>
-      <Home/>
+      <Routes />
     </>
   );
 }
